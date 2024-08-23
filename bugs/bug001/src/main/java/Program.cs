7b56e905448a410a73bddc09d6293e7c1c746365
mmazas <mmazas@ITEM-S104678.emea.msad.sopra>
2024-08-23 18:18:23 +0200

    Laundry on /bugs test cases (java, cpp, charp)
    Signed-off-by: mmazas <mmazas@ITEM-S104678.emea.msad.sopra>

--- bugs/bug001/src/main/java/Program.cs
+++ bugs/bug001/src/main/java/Program.cs
@@ -1,46 +1,64 @@
 ﻿using System;
-using System.Collections.Generic;
+//using System.Collections.Generic;
 using System.IO;
-using System.Linq;
+//using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+//using System.Threading.Tasks;
 
-namespace Bug002
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            TextReader input = Console.In;
-            TextWriter output = Console.Out;
-            TextWriter error = Console.Error;
-            TextReader prevInput = null;
-            TextWriter prevOutput = null;
-            TextWriter prevError = null;
-            if (args.Length == 3)
-            {
-                prevInput = input; input = new StreamReader(args[0]);
-                prevOutput = output; output = new StreamWriter(args[1]);
-                prevError = error; error = new StreamWriter(args[2]);
-                Console.SetIn(input);
-                Console.SetOut(output);
-                Console.SetError(error);
-            }
-            try
-            {
-                ComplexLineComment parser = new ComplexLineComment(input);
-                parser.Input();
-            }
-            catch (Exception e)
-            {
-                error.Write(e.Message);
-            }
-            finally
-            {
-                if (prevInput != null) Console.SetIn(prevInput);
-                if (prevOutput != null) Console.SetOut(prevOutput);
-                if (prevError != null) Console.SetError(prevError);
-            }
-        }
+namespace Bug001 {
+  
+  class Program {
+    
+    static void Main(string[] args) {
+      
+      if (args.Length != 3) {
+        Console.Error.WriteLine("Error: invalid number of arguments (" + args.Length + ")");
+        Console.Error.WriteLine("Usage: ComplexLineComment inputfile outputfile errorfile");
+        return;
+      }
+      
+      String fn = null;
+      TextReader stdInput = null;
+      TextWriter stdOutput = null;
+      TextWriter stdError = null;
+      StreamWriter fOutput = null;
+      StreamWriter fError = null;
+      try {
+        // open files and redirect standard streams to them
+        fn = "input";
+        stdInput = Console.In;
+        Console.SetIn(new StreamReader(args[0]));
+        fn = "output";
+        stdOutput = Console.Out;
+        fOutput = new StreamWriter(args[1]);
+        fOutput.AutoFlush = true;
+        Console.SetOut(fOutput);
+        fn = "error";
+        stdError = Console.Error;
+        fError = new StreamWriter(args[2]);
+        fError.AutoFlush = true;
+        Console.SetError(fError);
+        // parse
+        ComplexLineComment parser = new ComplexLineComment(Console.In);
+        parser.Input();
+        Console.Error.WriteLine("Input file parsed successfully");
+      } catch (IOException e) {
+        Console.Error.WriteLine("Error opening " + fn + " file");
+        Console.Error.WriteLine(e.Message);
+      } catch (Exception e) {
+        Console.Error.WriteLine("Error parsing input file");
+        Console.Error.WriteLine(e.Message);
+      } finally {
+        Console.In.Close();
+        Console.Out.Close();
+        Console.Error.Close();
+        if (stdInput  != null) Console.SetIn(stdInput);
+        if (stdOutput != null) Console.SetOut(stdOutput);
+        if (stdError  != null) Console.SetError(stdError);
+      }
+      
     }
+    
+  }
+  
 }
